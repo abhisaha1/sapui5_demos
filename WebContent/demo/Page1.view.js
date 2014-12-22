403f4aabf9abf4721f5cfcd3cd9a41330acf882d
Abhishek Saha <abhisheksaha11@gmail.com>
2014-12-22 23:04:11 +0530

    Demo1 - Navigation using Shell

---
+++ WebContent/demo/Page1.view.js
@@ -0,0 +1,25 @@
+sap.ui.jsview("demo.Page1", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf demo.Page1
+	*/ 
+	getControllerName : function() {
+		return "demo.Page1";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf demo.Page1
+	*/ 
+	createContent : function(oController) {
+		
+		var oText = new sap.ui.commons.TextView({
+			text: "This is Page1"
+		})
+		
+		return oText;
+		
+	}
+
+});
---
+++ WebContent/demo/Page2.view.js
@@ -0,0 +1,23 @@
+sap.ui.jsview("demo.Page2", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf demo.Page2
+	*/ 
+	getControllerName : function() {
+		return "demo.Page2";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf demo.Page2
+	*/ 
+	createContent : function(oController) {
+		var oText = new sap.ui.commons.TextView({
+			text: "This is Page2"
+		})
+		
+		return oText;
+	}
+
+});
